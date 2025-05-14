286600075f047cb4c0871bb1d59a5bd4161b5453
Singgih Tulus Makmud <133790648+sing2408@users.noreply.github.com>
2025-05-14 23:37:58 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,9 +17,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        path: "Sources/CorePackage",
         .target(
             name: "CorePackage",
+            path: "Sources/CorePackage",
             dependencies: [.process("Data/Models/GamersHubDataModel.xcdatamodeld")]
         ),
         .testTarget(
