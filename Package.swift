28e14bfa7b03a1a9282ba5ee21ffaf195175602e
Singgih Tulus Makmud <133790648+sing2408@users.noreply.github.com>
2025-05-14 22:29:33 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CorePackage",
-            dependencies: []
+            dependencies: [.process("Data/Models/GamersHubDataModel.xcdatamodeld")]
         ),
         .testTarget(
             name: "CorePackageTests",
